b0201ece39e169a0ddf22d9215becda28f0752d4
Gentrit Jashari <gentijashari5@gmail.com>
2022-02-06 12:28:04 +0100

    error field name mistake

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -124,7 +124,7 @@ export class UserResolver {
       return {
         errors: [
           {
-            field: "username",
+            field: "password",
             message: "incorrect password",
           },
         ],
